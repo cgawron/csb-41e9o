f6414d4e94a112e7cc327e4fe2bef807d5a43c2d
Code Sandbox <hello@codesandbox.io>
2021-11-05 20:06:19 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.96c16e0a624575c0c233ec9b880a2566.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7d7839caec9b907e68644db42af7b5",
+    "url": "https://cgawron.github.io/csb-41e9o/index.html"
+  },
+  {
+    "revision": "775625b3044c6cdc54ca",
+    "url": "https://cgawron.github.io/csb-41e9o/static/css/main.0e734603.chunk.css"
+  },
+  {
+    "revision": "2c72789e682b840587fe",
+    "url": "https://cgawron.github.io/csb-41e9o/static/js/2.8c81e16c.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "https://cgawron.github.io/csb-41e9o/static/js/2.8c81e16c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "775625b3044c6cdc54ca",
+    "url": "https://cgawron.github.io/csb-41e9o/static/js/main.5c602a7a.chunk.js"
+  },
+  {
+    "revision": "27be5b0047b5fe29fec8",
+    "url": "https://cgawron.github.io/csb-41e9o/static/js/runtime-main.73506776.js"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "https://cgawron.github.io/csb-41e9o/static/media/logo.be622899.svg"
+  }
+]);
